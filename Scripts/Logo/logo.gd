3241dfc9b0bae9e4d227eaa72a2f74f81b45d0fb
xUnD3athx <1132668457@qq.com>
2025-12-05 22:33:41 +0800

    feat: animation

---
+++ Scripts/Logo/logo.gd
@@ -0,0 +1,14 @@
+extends Control
+
+@onready var animation_player = $AnimationPlayer
+
+func _ready() -> void:
+	animation_player.play("logo_animation")
+
+func on_animation_finished():
+	get_tree().change_scene_to_file("res://Scenes/Main.tscn")
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_type():
+		if animation_player.is_playing():
+			animation_player.seek(4)
