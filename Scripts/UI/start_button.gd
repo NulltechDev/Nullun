ad8f5a6761bff823ec78a7da5955fc9824e5402f
xUnD3athx <1132668457@qq.com>
2025-12-06 15:16:19 +0800

    feat: animation

--- Scripts/UI/start_button.gd
+++ Scripts/UI/start_button.gd
@@ -3,6 +3,7 @@ extends TextureButton
 func _ready() -> void:
 	disabled = true
 	$AnimationPlayer.play("start_button_init")
+	$AnimationPlayer2.play("button_animation")
 
 func _on_pressed() -> void:
 	$AnimationPlayer.play("start_button_pressed")
